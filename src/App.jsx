782a1446304d16559f5b7c9f878eb4161d82a5bf
XIII <minibrusp@gmail.com>
2023-05-30 06:28:46 +0800

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1 className="text-sky-900 text-3xl font-bold underline">
+        Hello World!
+      </h1>
+    </div>
+  )
+}
+
+export default App
