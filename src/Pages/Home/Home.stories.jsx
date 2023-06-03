680ee6848dbabcd7b9f4f1f2470e25b0d2ca5f5f
XIII <minibrusp@gmail.com>
2023-06-04 01:03:27 +0800

    - Added Home Page story to show figma design addon - Added interaction test on home story

--- src/Pages/Home/Home.stories.jsx
+++ src/Pages/Home/Home.stories.jsx
@@ -1,6 +1,9 @@
 import Home from "./Home"
 import { FormContextProvider } from "../../Context/FormContext"
 
+import { userEvent, within } from '@storybook/testing-library'
+import { expect } from "@storybook/jest"
+
 export default {
   component: Home,
   title: "Pages/Home",
@@ -20,7 +23,15 @@ export default {
   ],
 }
 
-export const HomePage = {}
+export const HomePage = {
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/pqBRoDpWnjWNAZIlhU2ms7/Untitled?type=design&node-id=139-2&t=k0pNZH6ueL4YjbTJ-4",
+      allowFullscreen: true,
+    }
+  }
+}
 
 export const Mobile = {
   parameters: {
@@ -47,13 +58,74 @@ export const Desktop = {
     viewport: {
       defaultViewport: "FEMDESKTOP"
     },
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/pqBRoDpWnjWNAZIlhU2ms7/Untitled?type=design&node-id=90-8&t=KGEueDPhXgILtpAD-4",
+      allowFullscreen: true,
+    },
   },
 }
 
-Desktop.parameters = {
-  design: {
-    type: "figma",
-    url: "https://www.figma.com/file/pqBRoDpWnjWNAZIlhU2ms7/Untitled?type=design&node-id=90-8&t=KGEueDPhXgILtpAD-4",
-    allowFullscreen: true,
-  },
-};
+
+export const TestEmailFormExist = {
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement)
+
+    const label = canvas.getByText(/email address/i)
+    const textInput = canvas.getByPlaceholderText(/email@company.com/i)
+    const submitBtn = canvas.getByRole('button', { name: /subscribe/i })
+
+    await expect(submitBtn).toBeInTheDocument()
+    await expect(label).toBeInTheDocument()
+    await expect(textInput).toBeInTheDocument()
+  }
+}
+
+export const TestEmptySubmit = {
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement)
+
+    const textInput = canvas.getByPlaceholderText(/email@company.com/i)
+    const submitBtn = canvas.getByRole('button', { name: /subscribe/i })
+
+    await userEvent.click(submitBtn)
+
+    await expect(canvas.getByText(/empty field/i)).toBeInTheDocument()
+    await expect(textInput).toHaveStyle("border-color: hsl(4 100% 67% / 50 )")
+
+  }
+}
+
+export const TestInvalidSubmit = {
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement)
+
+    const textInput = canvas.getByPlaceholderText(/email@company.com/i)
+    const submitBtn = canvas.getByRole('button', { name: /subscribe/i })
+
+    await userEvent.type(textInput, "email@company")
+    await userEvent.click(submitBtn)
+
+    await expect(canvas.getByText(/valid email required/i)).toBeInTheDocument()
+    await expect(textInput).toHaveStyle("border-color: hsl(4 100% 67% / 50 )")
+
+  }
+}
+
+export const TestSuccessfullSubmit = {
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement)
+
+    const textInput = canvas.getByPlaceholderText(/email@company.com/i)
+    const submitBtn = canvas.getByRole('button', { name: /subscribe/i })
+
+    await userEvent.type(textInput, "email@company.com")
+    await userEvent.click(submitBtn)
+
+    await expect(canvas.getByAltText(/check icon/i)).toBeInTheDocument()
+    await expect(canvas.getByText(/thanks for subscribing/i)).toBeInTheDocument()
+    await expect(canvas.getByText(/email@company.com/i)).toBeInTheDocument()
+    await expect(canvas.getByRole('button', {name: /dismiss/i,})).toBeInTheDocument()
+
+  }
+}
