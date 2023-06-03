966a9a1a2e08f89ce04adde2689de03540360149
XIII <minibrusp@gmail.com>
2023-06-04 07:57:51 +0800

    Added footer, change favicon and edited window title

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ function App() {
     <div className="App">
       <FormContextProvider>
         <Home />
+        <footer class="attribution md:bg-neutral-charcoal-grey font-roboto text-sm w-full text-center tracking-widest m-0 p-4">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer" className="hover:text-neutral-white text-primary-tomato">Frontend Mentor</a>. 
+          Coded by <a href="https://www.frontendmentor.io/profile/minibrusp" target="_blank" rel="noreferrer" className="hover:text-neutral-white text-primary-tomato">XIII</a>.
+        </footer>
       </FormContextProvider>
     </div>
   )
