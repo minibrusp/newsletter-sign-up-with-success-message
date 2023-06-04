6122d84804202224713a0c2d9857e9986df659be
XIII <minibrusp@gmail.com>
2023-06-04 09:17:22 +0800

    edited storybook

---
+++ .storybook/manager.js
@@ -0,0 +1,19 @@
+import { addons } from '@storybook/manager-api';
+import myTheme from './myTheme';
+
+addons.setConfig({
+  showNav: true,
+  theme: myTheme,
+  panelPosition: 'bottom',
+  enableShortcuts: true,
+  showToolbar: true,
+  showPanel: true,
+  panelPosition: right,
+  sidebar: {
+    showRoots: true,
+    collapsedRoots: ['other'],
+  },
+  initialActive: 'sidebar',
+  selectedPanel: ,
+  
+});
---
+++ .storybook/myTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  // base: 'light',
+  brandTitle: 'Newsletter sign-up with success message',
+  brandUrl: 'https://newsletter-sign-up-with-success-message-frontend.vercel.app/',
+  brandImage: 'https://newsletter-sign-up-with-success-message-frontend.vercel.app/favicon.png',
+  brandTarget: '_self',
+});
